bf4a7146dd5c08bd4657c12b3e8b65af51bfa8d2
mxmoss <mxmossman@gmail.com>
2016-11-29 23:27:56 -0800

    Fix serializers so that we can PUT and POST without some fields
    Fix serializers so that we can PUT and POST without some fields
    Previously Theaters was requiring Films
    And Films was requiring Theaters and Genre
    But we don't always need to include those values on a PUT

--- films/serializers.py
+++ films/serializers.py
@@ -2,29 +2,27 @@ from rest_framework import serializers
 from films.models import Genre, Film, Theater
 
 class FilmSerializer(serializers.ModelSerializer):
-    theater_set = serializers.StringRelatedField(many=True)
-    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
-#    genre = serializers.StringRelatedField()
+    theater_set = serializers.StringRelatedField(many=True, required=False, )
+    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True, required=False,)
     class Meta:
         model = Film
         fields = ('id', 'title', 'year_prod', 'genre', 'theater_set')
 
 class GenreSerializer(serializers.ModelSerializer):
-    film_set = FilmSerializer(many=True)
+    film_set = FilmSerializer(many=True, required=False,)
     class Meta:
         model = Genre
         fields = ('id', 'description', 'film_set')
 
 class TheaterSerializer(serializers.ModelSerializer):
-    films = serializers.PrimaryKeyRelatedField(many=True, queryset=Film.objects.all(), allow_null=True)
+    films = serializers.PrimaryKeyRelatedField(many=True, queryset=Film.objects.all(), allow_null=True, required=False,)
     class Meta:
         model = Theater
         fields = ('id', 'name', 'city', 'state', 'num_screens', 'digital', 'comment_txt', 'films')
 
-class FilmWriteSerializer(serializers.ModelSerializer):
-    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
-#    genre = serializers.FilmSerializer( many=True)
-
-    class Meta:
-        model = Film
-        fields = ('id', 'title', 'year_prod', 'genre')
+# class FilmWriteSerializer(serializers.ModelSerializer):
+#     genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
+#
+#     class Meta:
+#         model = Film
+#         fields = ('id', 'title', 'year_prod', 'genre')
