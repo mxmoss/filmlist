491e2b0812b30bf8f4e0cdc71fab5ddc9d08906b
mxmoss <mxmossman@gmail.com>
2016-12-12 19:54:07 -0800

    attempting to put in pagination, but it's not working

--- filmlist/settings.py
+++ filmlist/settings.py
@@ -97,6 +97,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 3,
+# }
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
